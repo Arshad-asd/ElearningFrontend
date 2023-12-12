3f4cffdb9bf7e7456fc7721d073fc6cc9b9c9703
Arshad <arshaarshad21@gmail.com>
2023-12-12 15:08:23 +0530

    update baseurl

--- src/Containers/Utils/urls.js
+++ src/Containers/Utils/urls.js
@@ -1,12 +1,11 @@
 
 
-// const Protocol = window.location.protocol;
-// const BaseDomain = 'blearning.website';
-// const baseUrl = `${Protocol}//${BaseDomain}`;
+const Protocol = window.location.protocol;
+const BaseDomain = 'blearning.website';
+const baseUrl = `${Protocol}//${BaseDomain}`;
 
   // export const baseUrl = "http://127.0.0.1:8000";
 
-   export const baseUrl = "http://blearning.website";
 
   export const signup="/api/user/register/";
   export const tutorSignup = "/api/tutor/register";
